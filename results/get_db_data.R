462524cd563c1b47c6a25601025afb4b1f0d141c
Stefan Munnes <munnes@wzb.eu>
2025-02-12 22:30:46 +0100

    get data from db and create backup

---
+++ results/get_db_data.R
@@ -0,0 +1,32 @@
+
+# load database connection details from .env file
+dotenv::load_dot_env(".env")
+
+# connect to database
+pool <- pool::dbPool(
+  RPostgres::Postgres(),
+  host = Sys.getenv("SD_HOST"),
+  dbname = Sys.getenv("SD_DBNAME"),
+  port = Sys.getenv("SD_PORT"),
+  user = Sys.getenv("SD_USER"),
+  password = Sys.getenv("SD_PASSWORD"),
+  gssencmode = Sys.getenv("SD_GSSENCMODE", "prefer")
+)
+
+# load data from database table
+data <- DBI::dbReadTable(pool, Sys.getenv("SD_TABLE")) # 
+
+
+# close connection
+pool::poolClose(pool)
+
+
+# write data to disk as backup
+write.csv(
+  data, 
+  paste(
+    "results/data/db", "_",
+    Sys.getenv("SD_TABLE"), "_",
+    format(Sys.time(), "%Y%m%d_%H%M"), ".csv"
+  )
+)
