72abc0ea668d68be33caf06fdeffb4103af0a714
Stefan Munnes <munnes@wzb.eu>
2025-06-24 14:10:12 +0200

    formatting, clean variable add_to from whitespace, distinct label

--- items/add_checked_items_to_active.R
+++ items/add_checked_items_to_active.R
@@ -1,4 +1,3 @@
-
 ### add ckecked user input statements to active items excel list
 
 library(dplyr)
@@ -10,27 +9,34 @@ questions <- c("society", "region", "work")
 
 file_check <- "items/items_check.xlsx"
 file_active <- "survey/items_active.xlsx"
-files_check_backup <- list.files("items/backup", "^items_check_", full.names = TRUE)
+files_check_backup <- list.files(
+  "items/backup",
+  "^items_check_",
+  full.names = TRUE
+)
 
 
 # check if check file exists, then process inputs and create backup
 if (!file.exists(file_check)) {
   stop("Check file does not exist.")
 } else {
-
   # 1. load checked items and prepare the once to add to active items list
   message("Load checked items and prepare for adding to active items list.")
 
   # load checked items (if available for question)
   items_check_all <- lapply(questions, function(q) {
-    tryCatch({
-      openxlsx::read.xlsx(file_check, q, detectDates = TRUE) |>
-        mutate(across(everything(), as.character))
-    }, error = function(e) {
-      message(paste0("No checked items for question: ", q))
-      data.frame()
-    })
-  }) |> bind_rows()
+    tryCatch(
+      {
+        openxlsx::read.xlsx(file_check, q, detectDates = TRUE) |>
+          mutate(across(everything(), as.character))
+      },
+      error = function(e) {
+        message(paste0("No checked items for question: ", q))
+        data.frame()
+      }
+    )
+  }) |>
+    bind_rows()
 
   # error message if no add_to and no reason provided against add
   if (nrow(filter(items_check_all, is.na(add_to) & is.na(reason))) > 0) {
@@ -39,9 +45,9 @@ if (!file.exists(file_check)) {
 
   # prepare items for active items list
   items_check_keep <- items_check_all |>
-    # keep just statements that should be added to at least one question 
+    # keep just statements that should be added to at least one question
     filter(!is.na(add_to)) |>
-    # use corrected statement instead of original one, if provided 
+    # use corrected statement instead of original one, if provided
     mutate(label = ifelse(is.na(label_new), label, label_new)) |>
     select(add_to, label) |>
     # add helper columns for active items list
@@ -49,14 +55,16 @@ if (!file.exists(file_check)) {
       prob = 1,
       time_added = date_time,
     ) |>
-    tidyr::separate_rows(add_to, sep = ",") |> 
+    tidyr::separate_rows(add_to, sep = ",") |>
+    mutate(add_to = trimws(add_to)) |>
     distinct(add_to, label, .keep_all = TRUE)
 
   # error message if include not match question names
   if (!all(unique(items_check_keep$add_to) %in% questions)) {
     stop(
       "Invalid entry in add_to column (needs to be one of: ",
-      paste0(questions, collapse = ", "), ")."
+      paste0(questions, collapse = ", "),
+      ")."
     )
   }
 
@@ -66,14 +74,15 @@ if (!file.exists(file_check)) {
 
     # 1.2 load and add new checked items to keep to active items exel files
     items_active_new_3 <- lapply(questions, function(q) {
-
       # 1.2 load and add new checked items to active items exel files
       items_active <- readxl::read_xlsx(file_active, sheet = q)
 
       items_active_new <- bind_rows(
-        items_active, items_check_keep[items_check_keep$add_to == q, ]
+        items_active,
+        items_check_keep[items_check_keep$add_to == q, ]
       ) |>
-        mutate(item = ifelse(is.na(item), row_number(), item)) |> 
+        distinct(label, .keep_all = TRUE) |>
+        mutate(item = ifelse(is.na(item), row_number(), item)) |>
         select(!add_to)
 
       # check if item numbers are unique
@@ -96,41 +105,49 @@ if (!file.exists(file_check)) {
 
     # write new items to active
     writexl::write_xlsx(items_active_new_3, file_active)
-
-  } else{
+  } else {
     message("No new items to add to active items list.")
   }
 
   # 2. write all checked items to backup
   items_backup_new <- lapply(questions, function(q) {
-    
     # load checked items (if available for question)
-    items_check <- tryCatch({
-      openxlsx::read.xlsx(file_check, q, detectDates = TRUE) |>
-        mutate(across(everything(), as.character))
-    }, error = function(e) {
-      data.frame()
-    })
+    items_check <- tryCatch(
+      {
+        openxlsx::read.xlsx(file_check, q, detectDates = TRUE) |>
+          mutate(across(everything(), as.character))
+      },
+      error = function(e) {
+        data.frame()
+      }
+    )
 
     # load most recent backup file
     items_backup <- openxlsx::read.xlsx(
-      sort(files_check_backup, decreasing = TRUE)[1], q, detectDates = TRUE
+      sort(files_check_backup, decreasing = TRUE)[1],
+      q,
+      detectDates = TRUE
     )
 
     # combine backup and checked items to write again in file
     items_backup_new <- bind_rows(items_backup, items_check) |>
-      mutate(time_processed = ifelse(is.na(time_processed), date_time, time_processed))
+      mutate(
+        time_processed = ifelse(
+          is.na(time_processed),
+          date_time,
+          time_processed
+        )
+      )
 
     return(items_backup_new)
   })
 
   names(items_backup_new) <- questions
 
-
   message("Write processed checked items to backup and remvove check file.")
 
   writexl::write_xlsx(
-    items_backup_new, 
+    items_backup_new,
     paste0("items/backup/items_check_", date_time, ".xlsx")
   )
 
